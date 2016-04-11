32f7b0b721e17fe8d0a971fca9e70714e0c8296c
RosanneE <anderson.rosanne4@gmail.com>
2016-04-11 14:08:35 -0400

    Create E3point2.py

---
+++ E3point2.py
@@ -0,0 +1,24 @@
+#The prime factors of 13195 are 5, 7, 13 and 29.
+#What is the largest prime factor of the number 600851475143 ?
+
+#prime returns the largest prime number
+prime = 0
+#num is the number we are finding the prime of
+num = 600851475143
+#curr is the current number
+curr = 600851475143 - 1
+#count counts up to curr to check if curr is prime
+
+#end program when prime != 0
+while prime == 0:
+
+#If curr Goes into prime with no remainder, curr could be prime
+ if curr % num ==0:
+ while coun < curr
+ 
+#If curr has a remainder
+else:
+curr -=
+  
+ 
+

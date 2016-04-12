0a323009f4c4a146e25516519f18512e0434ed21
RosanneE <anderson.rosanne4@gmail.com>
2016-04-12 16:35:21 -0400

    Update E3point2.py

--- E3point2.py
+++ E3point2.py
@@ -8,17 +8,36 @@ num = 600851475143
 #curr is the current number
 curr = 600851475143 - 1
 #count counts up to curr to check if curr is prime
+count = 2
 
 #end program when prime != 0
 while prime == 0:
-
+ if curr == 2:
+  print "SO WRONG"
 #If curr Goes into prime with no remainder, curr could be prime
  if curr % num ==0:
- while coun < curr
+  #check if curr is prime
+  while count <= curr:
+ #curr is not prime
+   if curr%count == 0:
+    curr = cur - 1
+   # Keep checking for prime
+   elif count == curr:
+    prime = curr
+    print prime
+   else:
+    count = count + 1
+    
+    
+ #If curr has a remainder
+ else:
+  curr = curr -1 
+  
+print prime 
+
  
-#If curr has a remainder
-else:
-curr -=
+ 
+
   
  
 

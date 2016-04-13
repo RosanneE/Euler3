50d1344cad17642fd8d16c6ceee90d172914f4bf
RosanneE <anderson.rosanne4@gmail.com>
2016-04-13 13:46:15 -0400

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,8 @@
+#make a list to hold all numbers through high
+list1 = []
+#make high
+
+#make counter
+# populate list with numbers through high
+# % by number in space 0, ping counter if any = 0, delete all %0 number
+#return last prime
